ba8f70b5989cb349f9291fdfdb58e3631143da8c
Bruno Martins <bgcngm@gmail.com>
2018-06-07 09:41:11 +0100

    xiaomi/sdm845: Add initial device tree files
     * dipper device
     * Initial approach for using prebuilt vendor images.
    
    Change-Id: Ic9e562fae446cb287d7495d6b6bfae816b1b7b47
    
    Co-authored-by: argraur <argraur@gmail.com>

---
+++ device-common.mk
@@ -0,0 +1,63 @@
+#
+# Copyright (C) 2017 The Android Open-Source Project
+# Copyright (C) 2018 The LineageOS Project
+# Copyright (C) 2018 Pixel3ROM Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+LOCAL_PATH := device/xiaomi/sdm845
+
+# Overlays
+DEVICE_PACKAGE_OVERLAYS += \
+    $(LOCAL_PATH)/overlay
+
+# Audio
+PRODUCT_PACKAGES += \
+    audio.a2dp.default
+
+# Display
+PRODUCT_PACKAGES += \
+    libvulkan
+
+# IMS
+PRODUCT_PACKAGES += \
+    ims-ext-common
+
+# NFC
+PRODUCT_PACKAGES += \
+    com.android.nfc_extras \
+    NfcNci \
+    Tag
+
+# Net
+PRODUCT_PACKAGES += \
+    netutils-wrapper-1.0
+
+# RCS
+PRODUCT_PACKAGES += \
+    rcs_service_aidl \
+    rcs_service_aidl.xml \
+    rcs_service_api \
+    rcs_service_api.xml
+
+# Telephony
+PRODUCT_PACKAGES += \
+    telephony-ext
+
+PRODUCT_BOOT_JARS += \
+    telephony-ext
+
+# VNDK
+PRODUCT_PACKAGES += \
+    vndk_package
