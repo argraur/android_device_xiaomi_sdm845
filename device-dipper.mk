55b9f88ff4de977a42009bcaaf5a1482a8fb973c
Bruno Martins <bgcngm@gmail.com>
2018-06-08 15:29:28 +0100

    dipper: Add device input configs and keylayouts
    Change-Id: I848ecfd5f54f7ca6c9dd2355ad550cf1d7467cdc

--- device-dipper.mk
+++ device-dipper.mk
@@ -28,5 +28,15 @@ DEVICE_PACKAGE_OVERLAYS += \
 PRODUCT_AAPT_CONFIG := normal
 PRODUCT_AAPT_PREF_CONFIG := xxhdpi
 
+# Input
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/uinput-fpc.idc:system/usr/idc/uinput-fpc.idc \
+    $(LOCAL_PATH)/uinput-goodix.idc:system/usr/idc/uinput-goodix.idc
+
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/gpio-keys.kl:system/usr/keylayout/gpio-keys.kl \
+    $(LOCAL_PATH)/uinput-fpc.kl:system/usr/keylayout/uinput-fpc.kl \
+    $(LOCAL_PATH)/uinput-goodix.kl:system/usr/keylayout/uinput-goodix.kl
+
 # Inherit from common sdm845 device config
 $(call inherit-product, device/xiaomi/sdm845/device-common.mk)
